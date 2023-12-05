4f626b3d21992bb71dad633e94a0700b6b722a0d
Little Gao <98456232+LittleGaofx@users.noreply.github.com>
2023-12-05 20:31:39 +0800

    Add files via upload

--- wallpaper.py
+++ wallpaper.py
@@ -48,46 +48,50 @@ else:
 
 
 #获取外国区壁纸
-api_url_global = "https://global.bing.com/HPImageArchive.aspx?n=1&setmkt=%s&setlang=en&idx=0&format=js"
-response = requests.get(api_url_global)
-if response.status_code == 200:
+RegionList = ["en-US", "ja-JP", "en-IN", "pt-BR", "fr-FR", "de-DE", "en-CA", "en-GB", "it-IT", "es-ES", "fr-CA"]
+
+for region in RegionList:
+
+    api_url_global = f"https://global.bing.com/HPImageArchive.aspx?n=1&setmkt={region}&setlang=en&idx=0&format=js"
+    response = requests.get(api_url_global)
+    if response.status_code == 200:
     
-    data = response.json()
-    urlbase = data["images"][0]["urlbase"]
-    url = "https://www.bing.com" + urlbase + "_UHD.jpg"
-    image_data = requests.get(url).content
-    image_path = os.path.join("global","img","background.jpg")
-    os.makedirs(os.path.dirname(image_path), exist_ok=True)
-    with open(image_path, "wb") as f:
-        f.write(image_data)
-        f.close()
-    print(f"图片已经保存到 {image_path}")
+        data = response.json()
+        urlbase = data["images"][0]["urlbase"]
+        url = "https://www.bing.com" + urlbase + "_UHD.jpg"
+        image_data = requests.get(url).content
+        image_path = os.path.join("global",region,"img","background.jpg")
+        os.makedirs(os.path.dirname(image_path), exist_ok=True)
+        with open(image_path, "wb") as f:
+            f.write(image_data)
+            f.close()
+        print(f"图片已经保存到 {image_path}")
 
-    enddate = data["images"][0]["enddate"]
-    yy = f"{enddate[:4]}"
-    mm = f"{enddate[4:6]}"
-    dd = f"{enddate[6:]}"
-    enddate = f"{enddate[:4]}-{enddate[4:6]}-{enddate[6:]}"
+        enddate = data["images"][0]["enddate"]
+        yy = f"{enddate[:4]}"
+        mm = f"{enddate[4:6]}"
+        dd = f"{enddate[6:]}"
+        enddate = f"{enddate[:4]}-{enddate[4:6]}-{enddate[6:]}"
 
-    copy = data["images"][0]["copyright"]
-    title = data["images"][0]["title"]
-    info_path = os.path.join("global","README.md")
-    with open(info_path, "w", encoding='utf-8') as f:
-        f.write(f"## Today's Bing Wallpaper\n")
-        f.write(f"Update: {enddate}\n")
-        f.write(f"![]({url}&w=1000)Download: [{copy}]({url})")
-        f.write(f"\n\nAuto get programm by LtgX\n")
-    print(f"信息已经保存到 {info_path}")
+        copy = data["images"][0]["copyright"]
+        title = data["images"][0]["title"]
+        info_path = os.path.join("global",region,"README.md")
+        with open(info_path, "w", encoding='utf-8') as f:
+            f.write(f"## Today's Bing Wallpaper\n")
+            f.write(f"Update: {enddate}\n")
+            f.write(f"![]({url}&w=1000)Download: [{copy}]({url})")
+            f.write(f"\n\nAuto get programm by LtgX\n")
+        print(f"信息已经保存到 {info_path}")
 
-    history_file_name = f"{dd}.md"
-    history_path = os.path.join("global","history",yy,mm,history_file_name)
-    os.makedirs(os.path.dirname(history_path), exist_ok=True)
-    with open(history_path, "w", encoding='utf-8') as f:
-        f.write(f"## History Bing Wallpaper\n")
-        f.write(f"Wallpaper date: {enddate}\n")
-        f.write(f"![]({url}&w=1000)Download: [{copy}]({url})")
-        f.write(f"\n\nAuto get programm by LtgX\n")
-    print(f"信息已经保存到 {history_path}")
+        history_file_name = f"{dd}.md"
+        history_path = os.path.join("global",region,"history",yy,mm,history_file_name)
+        os.makedirs(os.path.dirname(history_path), exist_ok=True)
+        with open(history_path, "w", encoding='utf-8') as f:
+            f.write(f"## History Bing Wallpaper\n")
+            f.write(f"Wallpaper date: {enddate}\n")
+            f.write(f"![]({url}&w=1000)Download: [{copy}]({url})")
+            f.write(f"\n\nAuto get programm by LtgX\n")
+        print(f"信息已经保存到 {history_path}")
     
-else:
-    print(f"请求失败，状态码为 {response.status_code}")
+    else:
+        print(f"请求失败，状态码为 {response.status_code}")
